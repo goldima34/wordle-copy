55829af3d6162f1cf14f0ec4e8537ae132a5bd74
goldima43 <golovaschukdima@gmail.com>
2024-08-09 17:01:23 +0300

    remake some style

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.scss";
+import './GlobalStyles/page.scss'
 import NavBar from "@/components/NavBar/NavBar";
 
 export default function Home() {
   return (
    <>
    <NavBar/>
-   <div className={styles.buttonsContainer}>
-      <a href="/game" className={styles.playBtn}>
-        <div className={styles.content}>
+   <div className="buttonsContainer">
+      <a href="/game" className="playBtn">
+        <div className="content">
           <h2>PLAY</h2>
           <h2>PLAY</h2>
         </div>
